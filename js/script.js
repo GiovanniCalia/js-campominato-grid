8fabd2c6e6c129c910a1e09941e3ab7ea40e4345
GiovanniCalia <johnca@outlook.it>
2022-03-02 16:42:12 +0100

    Continuazione esercizio, da riguardare

--- js/script.js
+++ js/script.js
@@ -1,5 +1,30 @@
-let areaBoxes = document.getElementById("container");
+const eleText = document.getElementById("text");
+const btnPlay = document.getElementById("play");
+const areaBoxes = document.getElementById("container");
+const btnEasy = document.getElementById("easy");
+const btnMedium = document.getElementById("medium");
+const btnHard = document.getElementById("hard");
+
 
 for (i = 1; i <= 100; i++){
     areaBoxes.innerHTML += `<div class="box">${i}</div>`;
 }
+
+btnPlay.addEventListener("click", easy);
+
+function easy(){
+    for (i = 1; i <= 100; i++){
+        areaBoxes.innerHTML += `<div class="box">${i}</div>`;
+    }
+}
+
+
+btnPlay.addEventListener("click", medium);
+
+function medium(){
+    for (i = 1; i <= 81; i++){
+        areaBoxes.innerHTML += `<div class="box">${i}</div>`;
+    }
+}
+
+
