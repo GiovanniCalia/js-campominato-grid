e03dbab372572b0171f0aa1dc9869f257f23be11
GiovanniCalia <johnca@outlook.it>
2022-03-02 23:53:33 +0100

    Continuazione esercizio, non riuscito

--- js/script.js
+++ js/script.js
@@ -1,18 +1,32 @@
 const eleText = document.getElementById("text");
 const btnPlay = document.getElementById("play");
-let areaBoxes = document.getElementById("container");
+let areaBoxesEasy = document.getElementById("container-easy");
+let areaBoxesMedium = document.getElementById("container-medium");
+let areaBoxesHard = document.getElementById("container-hard");
 const btnEasy = document.getElementById("easy");
 const btnMedium = document.getElementById("medium");
 const btnHard = document.getElementById("hard");
 
-
 for (i = 1; i <= 100; i++){
-    areaBoxes.innerHTML += `<div class="box">${i}</div>`;
+    areaBoxesEasy.innerHTML += `<div class="box">${i}</div>`;
+    console.log(areaBoxesEasy);
 }
 
+for (i = 1; i <= 81; i++){
+    areaBoxesMedium.innerHTML += `<div class="box">${i}</div>`;
+    console.log(areaBoxesMedium);
+    areaBoxesMedium.style.display = "none";
+}
 
-btnPlay.addEventListener("click", function(){
-    for (i = 1; i <= 100; i++){
-          areaBoxes.innerHTML += `<div class="box">${i}</div>`;
-       }
+for (i = 1; i <= 49; i++){
+    areaBoxesHard.innerHTML += `<div class="box">${i}</div>`;
+    console.log(areaBoxesHard);
+    areaBoxesHard.style.display = "none";
+}
+
+let found = false;
+btnPlay.addEventListener("click", function(){ 
+    if (areaBoxesMedium.style.display = "flex"){
+        found = true;
+    } 
 })
